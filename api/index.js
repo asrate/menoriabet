db792d1d977d0a1c534b30d34fc1239c0f96c6e6
asrate <asrate2121@yahoo.com>
2023-10-27 00:12:35 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => console.log(err));
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function (req, res) {
