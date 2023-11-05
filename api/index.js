7a39d655c92352054680d1a7913a83f4bd8e5f1e
asrate <asrate2121@yahoo.com>
2023-11-05 22:03:28 +0300

    complete signup page functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
+import cors from "cors";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -11,6 +12,7 @@ mongoose
   })
   .catch((err) => console.log(err));
 const app = express();
+app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 app.use("/api/user", userRouter);
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,31 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function SignUP() {
+  const [formdata, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formdata,
+      [e.target.id]: e.target.value,
+    });
+  };
+  console.log(formdata);
+  const handleSubmit = async (e) => {
+    try {
+      setLoading(true);
+      e.preventDefault();
+      const res = await fetch("http://localhost:5000/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formdata),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+      }
+
+      setLoading(false);
+      setError(null);
+      navigate("/signin");
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+  // console.log(formdata);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg "
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg "
           id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg "
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-gray-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 ">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-gray-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 "
+        >
+          {loading ? "loading..." : "Sign Up"}
         </button>
       </form>
       <div>
@@ -36,6 +81,7 @@ function SignUP() {
           </Link>
         </p>
       </div>
+      {error && <p className="text-red-700">{error}</p>}
     </div>
   );
 }
